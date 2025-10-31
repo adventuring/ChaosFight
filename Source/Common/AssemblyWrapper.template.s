e3ba26e844bf9dad3bb45a1ddc8711f00318026d
Bruce-Robert Pocock <brpocock@star-hope.org>
2025-10-30 21:09:30 -0700

    Remove printf/echo from Makefile - use template file instead
    - Create AssemblyWrapper.template.s as source template
    - Use sed to generate TV-standard-specific wrappers
    - No more printf/echo appending to source files
    - Template file is version controlled and proper source code

---
+++ Source/Common/AssemblyWrapper.template.s
@@ -0,0 +1,9 @@
+; ChaosFight - Generated assembly wrapper template
+; This file is a template - actual wrappers are generated from this
+; TV_STANDARD will be replaced with NTSC/PAL/SECAM
+
+          include "Source/Common/AssemblyConfig.s"
+          include "Source/Common/AssemblyConfig.TV_STANDARD.s"
+          include "Source/Generated/ChaosFight.TV_STANDARD.body.s"
+          include "Source/Common/AssemblyFooter.s"
+

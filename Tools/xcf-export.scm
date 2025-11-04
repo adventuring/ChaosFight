1ecde55db30e278f57ace44ba88eee10ddb44d08
Bruce-Robert Pocock <brpocock@star-hope.org>
2025-11-04 14:16:47 -0800

    Fix GIMP script-fu deprecated PDB procedure calls (Issue #322)
    - Updated file-png-export to use named arguments instead of ordered arguments
    - Changed from: (file-png-export RUN-NONINTERACTIVE image dest 0 TRUE 9 ...)
    - Changed to: (file-png-export #:run-mode RUN-NONINTERACTIVE #:image image ...)
    - This eliminates the deprecation warning from GIMP script-fu
    - Maintains same functionality with future-compatible syntax
    
    All PDB procedure arguments now use #:keyword format as required by modern GIMP.

--- Tools/xcf-export.scm
+++ Tools/xcf-export.scm
@@ -1,5 +1,16 @@
 (define (xcf-export source dest)
   (let ((image (car (gimp-file-load RUN-NONINTERACTIVE source))))
-    (file-png-export RUN-NONINTERACTIVE image dest
-                     0 TRUE 9 TRUE TRUE TRUE TRUE FALSE TRUE "auto")
+    (file-png-export #:run-mode RUN-NONINTERACTIVE
+                     #:image image
+                     #:file dest
+                     #:options 0
+                     #:interlaced TRUE
+                     #:compression 9
+                     #:bkgd TRUE
+                     #:offs TRUE
+                     #:phys TRUE
+                     #:time TRUE
+                     #:save-transparent FALSE
+                     #:optimize-palette TRUE
+                     #:format "auto")
     (gimp-image-delete image)))

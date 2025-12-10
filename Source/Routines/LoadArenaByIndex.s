9bb8335e84fd64f8a8ad9ff10e45f30b4080b70b
Bruce-Robert Pocock <brpocock@star-hope.org>
2025-12-10 12:42:02 -0800

    Apply style guide v3.0 to LoadArenaByIndex.s
    LoadArenaByIndex.s:
    - Remove extra blank lines after .proc
    - Fix blank lines (exactly one after branch/jump/rts instructions)
    - Fix indentation of ldx/lda/sta instructions (10 spaces)
    - Remove orphaned labels (so:, arena_index:)
    - Add ':' suffix to MultiplyBy8 label

--- Source/Routines/LoadArenaByIndex.s
+++ Source/Routines/LoadArenaByIndex.s
@@ -4,7 +4,6 @@
 
 
 LoadArenaByIndex .proc
-
           ;; Load arena data by index into playfield RAM
           ;; Returns: Far (return otherbank)
           ;; Input: temp1 = arena index (0-31)
@@ -15,61 +14,55 @@ LoadArenaByIndex .proc
           ;; Calculate arena data pointer: Arena0Playfield + (arena_index × 24)
           ;; Each arena stores 16 bytes of playfield data followed by 8 bytes of row colors (16 + 8 = 24)
           ;; TODO: ; Optimized: Multiply by 24 = multiply by 8, then multiply by 3
-so:
-
-
-arena_index:
-
-
-            lda temp1
-            sta temp2
-            lda # 0
-            sta temp3
+          lda temp1
+          sta temp2
+          lda # 0
+          sta temp3
 
           ;; TODO: ; Multiply by 8 (3 shifts)
-                    ldx # 3
-MultiplyBy8:
+          ldx # 3
 
-            asl temp2
-            rol temp3
-            dex
-            bne MultiplyBy8
+MultiplyBy8:
+          asl temp2
+          rol temp3
+          dex
+          bne MultiplyBy8
 
           ;; TODO: ; Now multiply by 3: temp2/temp3 * 3 = temp2/temp3 + (temp2/temp3 * 2)
           ;; TODO: ; Store original value for addition
-            lda temp2
-            sta temp4
-            lda temp3
-            sta temp5
+          lda temp2
+          sta temp4
+          lda temp3
+          sta temp5
 
           ;; TODO: ; Multiply by 2 (1 shift)
-            asl temp2
-            rol temp3
+          asl temp2
+          rol temp3
 
           ;; TODO: ; Add original to get * 3
-            clc
-            lda temp2
-            adc temp4
-            sta temp2
-            lda temp3
-            adc temp5
-            sta temp3
+          clc
+          lda temp2
+          adc temp4
+          sta temp2
+          lda temp3
+          adc temp5
+          sta temp3
 
           ;; TODO: ; Add base address
-            lda # <.Arena0Playfield
-            clc
-            adc temp2
-            sta temp4
-            lda # >.Arena0Playfield
-            adc temp3
-            sta temp5
+          lda # <.Arena0Playfield
+          clc
+          adc temp2
+          sta temp4
+          lda # >.Arena0Playfield
+          adc temp3
+          sta temp5
 
-            lda temp4
-            sta PF1pointer
-            sta PF2pointer
-            lda temp5
-            sta PF1pointer+1
-            sta PF2pointer+1
+          lda temp4
+          sta PF1pointer
+          sta PF2pointer
+          lda temp5
+          sta PF1pointer+1
+          sta PF2pointer+1
           jsr BS_return
 
 .pend

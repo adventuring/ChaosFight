73de533d934ba5346aebd2d39f8b86310b700532
Bruce-Robert Pocock <brpocock@star-hope.org>
2025-12-10 00:45:21 -0800

    Apply style guide v3.0 to GetPlayerPosition.s, GetPlayerVelocity.s, SetPlayerPosition.s
    GetPlayerPosition.s:
    - Add ':' suffix to GetPlayerPosition label
    - Fix comment formatting (remove extra semicolons)
    - Remove extra blank lines at end
    
    GetPlayerVelocity.s:
    - Add ':' suffix to GetPlayerVelocity label
    - Fix corrupted line 13 (split multiple instructions onto separate lines)
    - Fix indentation (10 spaces)
    - Fix blank lines
    
    SetPlayerPosition.s:
    - Add ':' suffix to SetPlayerPosition label
    - Fix immediate values (add # and space: lda 0 → lda # 0)
    - Fix comment formatting (remove extra semicolons)
    - Remove extra blank lines at end

--- Source/Routines/GetPlayerPosition.s
+++ Source/Routines/GetPlayerPosition.s
@@ -1,19 +1,19 @@
 ;;; ChaosFight - Source/Routines/GetPlayerPosition.bas
 ;;; Copyright © 2025 Bruce-Robert Pocock.
 
-GetPlayerPosition
-;;; Get player position (integer components only).
+GetPlayerPosition:
+          ;; Get player position (integer components only).
           ;; Input: currentPlayer (global) = player index (0-3)
           ;; Output: temp2 = X position, temp3 = Y position
           ;; Mutates: temp2, temp3
           ;; Constraints: Callers should consume the values immediately; temps are volatile.
-                    let temp2 = playerX[currentPlayer]
+          ;; let temp2 = playerX[currentPlayer]
           lda currentPlayer
           asl
           tax
           lda playerX,x
           sta temp2
-                    let temp3 = playerY[currentPlayer]
+          ;; let temp3 = playerY[currentPlayer]
           lda currentPlayer
           asl
           tax
--- Source/Routines/GetPlayerVelocity.s
+++ Source/Routines/GetPlayerVelocity.s
@@ -2,20 +2,27 @@
 ;;; Copyright © 2025 Bruce-Robert Pocock.
 
 
-GetPlayerVelocity .proc
-;;; Get player velocity (integer components only).
+GetPlayerVelocity:
+.proc
+          ;; Get player velocity (integer components only).
           ;; Input: currentPlayer (global) = player index (0-3)
           ;; playerVelocityX[], playerVelocityY[] (global arrays)
           ;; Output: temp2 = X velocity, temp3 = Y velocity
           ;; Mutates: temp2, temp3
           ;; Constraints: Callers should use the values immediately; temps are volatile.
-                    let temp2 = playerVelocityX[currentPlayer]          lda currentPlayer          asl          tax          lda playerVelocityX,x          sta temp2
-                    let temp3 = playerVelocityY[currentPlayer]
+          ;; let temp2 = playerVelocityX[currentPlayer]
+          lda currentPlayer
+          asl
+          tax
+          lda playerVelocityX,x
+          sta temp2
+          ;; let temp3 = playerVelocityY[currentPlayer]
           lda currentPlayer
           asl
           tax
           lda playerVelocityY,x
-          sta temp3         
+          sta temp3
+
           lda currentPlayer
           asl
           tax

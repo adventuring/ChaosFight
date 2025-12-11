9c41ea0b1ca7ba6d4b08386312c328c7d21d947c
Bruce-Robert Pocock <brpocock@star-hope.org>
2025-12-10 17:19:05 -0800

    Remove outdated documentation comments
    - Removed 'was:' and 'no longer used' historical documentation from VblankHandlers.s
    - Documentation should describe current state, not historical changes

--- Source/Routines/PlayerInput.s
+++ Source/Routines/PlayerInput.s
@@ -404,10 +404,12 @@ InputDonePlayer3Input
 
           ;; Constraints: Must be colocated with InputHandleQuadtariPlayers
           lda controllerStatus
-          and SetQuadtariDetected
+          and # SetQuadtariDetected
           cmp # 0
           bne CheckPlayer4Character
+
           jmp InputDonePlayer4Input
+
 CheckPlayer4Character:
 
 
